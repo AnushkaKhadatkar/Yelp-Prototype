3f8a7bc033a658f637e1f60547e9f00e7c5c7119
Parthp28 <Patelparth4629@gmail.com>
2026-04-20 19:54:25 -0700

    [redux] expose favourites selectors
    Adds selector helpers for favourites list, ids, loading, and pending states to simplify page wiring for Week 3 flows.

--- frontend/src/selectors/favouritesSelectors.js
+++ frontend/src/selectors/favouritesSelectors.js
@@ -1,1 +1,6 @@
 export const selectFavouritesState = (state) => state.favourites
+export const selectFavouriteItems = (state) => state.favourites.items
+export const selectFavouriteIds = (state) => state.favourites.ids
+export const selectFavouritesLoading = (state) => state.favourites.loading
+export const selectFavouritesError = (state) => state.favourites.error
+export const selectFavouritesPendingById = (state) => state.favourites.pendingById
